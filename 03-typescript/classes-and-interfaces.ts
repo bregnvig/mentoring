c8e0ee2d511c4c7fd94d65a8a26f7f3288e07d35
Flemming Bregnvig <flemming@bregnvig.dk>
2019-12-09 20:51:01 +0100

    Prepare for 03-typescript

---
+++ 03-typescript/classes-and-interfaces.ts
@@ -0,0 +1,28 @@
+/**
+ * Lets start with interfaces
+ * Create a interface: Coordinate
+ * The coordinate interface has two properties lat and lng. Both type number
+ * 
+ * Create one more interface called Playground. 
+ * This interface needs four string properties:
+ * id, name, description & addressDescription
+ * Descript and address description are optional properties
+ * Finally a property called position, of type Coordinate
+ */
+
+
+ // Open the playgrounds.ts file, and explicitly set the type of the array to
+ // be a Playground[]. Make sure everything compiles
+
+ // Create a class called playground service. It should have a private instance variable
+ // called playgrounds. Assign the playground array to this instance variable.
+ // Combining some of the stuff from last time, create a method that takes an id and returns
+ // the playground with the given id.
+
+
+ // Create an instance of this class and call the function finding the playground with id 'legeplads.50'
+ // console.log the value.
+
+ // Our current position is 55.867945, 12.3610023. Add a method called 'closest' to the class, that finds the nearest playground.
+ // There is an exported function called getDistance in the playgrounds.ts file. Use this to find the playground
+  
---
+++ 03-typescript/simple-types.ts
@@ -0,0 +1,42 @@
+/**
+ * Working simple TypeScript 
+ */
+
+// Create an array of type string. Add two string to the array when you create it
+
+
+// Push a third string to the array
+
+
+// Push a number to the array. Save the fil and see the error message
+
+
+// Create two strings using let. One with explicit type, and one without. Notice if you hover you
+// mouse cursor above the one without an explicit type, it will still say string as type
+// Try changing let to const and see what the type is now, when hovering. (We will talk about that later) 
+
+// Create an object with two properties, name and descriptiion. Name is 'Legepladsen på Sundbyvester Plads',
+// description is 'I 2012 ombygges legepladsen og bliver en plads for store børn'
+
+// Try assigning a number to the name
+
+
+// Create a tuble a string and a number. Try to assign different value to it, and observe the compiler
+
+
+
+/**
+ * Everything is a type
+ * This is all about compiling
+ */
+
+// Remeber creating an implicit string using let? 
+// Now create an implicit string using const! How did the type change and why?
+
+
+// Create an object of implicit type, with property a and b. Assign any value to the properties.
+
+// Try to assign a new property c to the object?. Why didn't it work?
+
+// Change from implicit type to explicit type object. Can you add property c now?
+
